8460661c5614efe93c8e5d428a4ff410b6ff4670
mercatel <mercatel86@gmail.com>
2020-12-11 00:35:22 +0500

    + vuex + router

---
+++ src/store/modules/post.js
@@ -0,0 +1,14 @@
+export default {
+    state: {
+        state: {
+            posts:[],
+        }
+    },
+    actions: {},
+    mutations: {},
+    getters: {
+        allPosts(state){
+            return state.posts
+        }
+    },
+}
