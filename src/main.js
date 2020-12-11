f2b5592f859d782f3923756ba9f106fe0f808979
Dinard <mercatel86@gmail.com>
2020-12-11 13:34:46 +0500

    start(router, vuex, vuetify)

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
-import vuetify from '@/plugins/vuetify'
 import router from './router'
 import store from './store/vuex'
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
 
 new Vue({
   store,
-  vuetify,
   router,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
